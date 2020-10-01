5162d75cad2136f63d3ba5c39f2cea9c443bea90
Shotaro Hirose <shotaro199959@icloud.com>
2020-10-02 00:21:48 +0900

    [Add] ユーザ登録・認証機能

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,7 +3,7 @@
     <h2>Sign up</h2>
     <input type="text" placeholder="Username" v-model="username">
     <input type="text" placeholder="Password" v-model="password">
-    <button>Register</button>
+    <button @click="signUp">Register</button>
     <p>Do you have an account?
       <router-link to="/signin">sign in now!!</router-link>
     </p>
@@ -11,12 +11,25 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'Signup',
   data () {
     return {
       username: '',
       password: ''
+    }
+  },
+  methods: {
+    signUp () {
+      firebase.auth().createUserWithEmailAndPassword(this.username, this.password)
+        .then(user => {
+          alert('Create account: ', user.email)
+        })
+        .catch(error => {
+          alert(error.message)
+        })
     }
   }
 }
