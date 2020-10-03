a8bab6fc8bb8fc012c5a82501d5d264e4bcb82fb
Shotaro Hirose <shotaro199959@icloud.com>
2020-10-03 21:28:06 +0900

    [Add] サインアウト機能を実装

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <h1>Hello {{ name }}!!</h1>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -80,15 +81,26 @@
         </a>
       </li>
     </ul>
+    <button @click="signOut">Sign out</button>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      name: firebase.auth().currentUser.email
+    }
+  },
+  methods: {
+    signOut () {
+      firebase.auth().signOut().then(() => {
+        this.$router.push('/signin')
+      })
     }
   }
 }
